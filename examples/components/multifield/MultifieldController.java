1e7854be1cdb9c839a26d4f700a81e7d6cda199c
Jorge Chércoles <jorge.chercoles@vass.es>
2020-10-02 14:54:44 +0200

    Updated code to be the same as the other examples

--- examples/components/multifield/MultifieldController.java
+++ examples/components/multifield/MultifieldController.java
@@ -1,37 +1,44 @@
 package com.<project>.core.models;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 @Model(adaptables = Resource.class)
-public class PruebaMultiModel {
+public class MultiFieldController {
+
+    @ValueMapValue(name = PROPERTY_RESOURCE_TYPE, injectionStrategy = InjectionStrategy.OPTIONAL)
+    @Default(values = "No resourceType")
+    protected String resourceType;
 
     @SlingObject
     private Resource currentResource;
 
-    private ArrayList<String> list;
-
-    public ArrayList<String> getLista() {
-        return list;
-    }
+    private ArrayList<String> multi;
 
     @PostConstruct
     protected void init() {
-        this.list = new ArrayList<>();
-        String text;
+        this.multi = new ArrayList<>();
 
-        Resource multi = currentResource.getChild("list");
-        if (multi != null) {
-            Iterator<Resource> children = multi.listChildren();
+        Resource res_list = currentResource.getChild("multi");
+        if (res_list != null) {
+            Iterator<Resource> children = res_list.listChildren();
             while (children.hasNext()) {
-                text = children.next().getValueMap().get("text", String.class);
-                list.add(text);
+                Resource res_item = children.next();
+                if(res_item.getValueMap().containsKey("text"))
+                    this.multi.add(res_item.getValueMap().get("text", String.class));
             }
         }
+    }
+
+    public ArrayList<String> getMulti() {
+        return this.multi;
     }
 }
